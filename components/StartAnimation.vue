ba4415b670a016e4ea9631fea68ee85c6976e39a
Achraf <hamrouni.pro@outlook.fr>
2021-11-19 15:49:01 +0100

    fix video

--- components/StartAnimation.vue
+++ components/StartAnimation.vue
@@ -1,23 +1,7 @@
 <template>
   <div ref="sylphe_container" class="h-screen sylphe_container w-screen max-w-screen max-h-screen transition-all duration-700 ease-in-out fixed top-0 left-0" style="z-index: 999999999; background-color: #1E1F1D;">
     <video ref="sylphe" id="sylphe" muted playsinline preload="metadata" class="h-full w-full max-w-screen max-h-screen">
-      <source :src="video" type="video/webm">
+      <source :src="$md !== 'lg' ? '/sylphe_mobile_a.webm' : '/sylphe_a.webm'" type="video/webm">
     </video>
   </div>
 </template>
-
-
-<script>
-  export default {
-    data() {
-      return {
-        video: '/sylphe_a.webm'
-      }
-    },
-    mounted() {
-      this.$nextTick(() => {
-        this.video = parseInt(document.documentElement.clientWidth) >= 480 ? '/sylphe_a.webm' : '/sylphe_mobile_a.webm'
-      })
-    }
-  }
-</script>
